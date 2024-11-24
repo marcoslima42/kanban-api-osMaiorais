ae9f673ff6ade42e81ebe43ea2453784cd47ad67
barbrete <barbara.cobo@estudante.iftm.edu.br>
2024-11-24 19:55:21 -0300

    Fix: Entidade sem ID

--- src/main/java/com/bracme/kanban/kanban/entities/abstracts/BaseEntity.java
+++ src/main/java/com/bracme/kanban/kanban/entities/abstracts/BaseEntity.java
@@ -1,18 +1,22 @@
 package com.bracme.kanban.kanban.entities.abstracts;
 
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Data
 @AllArgsConstructor
@@ -21,6 +25,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @MappedSuperclass
 public abstract class BaseEntity implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false, unique = true)
     private UUID id;
 
