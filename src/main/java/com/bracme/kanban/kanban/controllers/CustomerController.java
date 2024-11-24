581474ac90969dd68f59393272dad05a03d6d15c
Henrique Pereira <116195064+Hhpp2004@users.noreply.github.com>
2024-11-24 18:40:19 -0300

    feat. troca de variavel na função de patch

--- src/main/java/com/bracme/kanban/kanban/controllers/CustomerController.java
+++ src/main/java/com/bracme/kanban/kanban/controllers/CustomerController.java
@@ -1,5 +1,18 @@
 package com.bracme.kanban.kanban.controllers;
 
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bracme.kanban.kanban.dtos.requests.CreateCustomerRequestDto;
@@ -11,20 +24,6 @@ import com.bracme.kanban.kanban.services.CustomerService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-
-import java.util.UUID;
-
-import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
 
 @RequiredArgsConstructor
 @RestController
@@ -89,6 +88,6 @@ public class CustomerController {
 		@RequestBody UpdatePasswordCustomerRequestDto dto) {
 
 		customerService.updatePasswordById(id, dto);
-		return ResponseEntity.noContent().build();
+		return ResponseEntity.ok(null);
 	}
 }
