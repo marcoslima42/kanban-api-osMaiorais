0f9219356a563e65229e15e7c34401e70f2db9c9
Pedro Rabis <rabis.pedro@gmail.com>
2024-11-24 00:11:58 -0300

    refactor: limpeza de código não utilizado

--- src/main/java/com/bracme/kanban/kanban/controllers/CustomerController.java
+++ src/main/java/com/bracme/kanban/kanban/controllers/CustomerController.java
@@ -2,20 +2,25 @@ package com.bracme.kanban.kanban.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bracme.kanban.kanban.dtos.requests.CreateCustomerRequestDto;
 import com.bracme.kanban.kanban.dtos.responses.CustomerResponseDto;
 import com.bracme.kanban.kanban.services.CustomerService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+
+import java.net.URI;
 
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@Log4j2
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(path = "/customers")
@@ -23,15 +28,21 @@ public class CustomerController {
 	private final CustomerService customerService;
 
 	@GetMapping("")
-	@Operation(summary = "Returns a list of all customers")
+	@Operation(summary = "Returns a list of all customers with pagination")
 	public ResponseEntity<Page<CustomerResponseDto>> getAll(
 		@RequestParam(name = "page", defaultValue = "0") Integer page,
 		@RequestParam(name = "size", defaultValue = "20") Integer size,
 		@RequestParam(name = "orderBy", defaultValue = "createdAt") String orderBy,
-		@RequestParam(name = "direction", defaultValue = "ASC") String direction
-	) {
-		var response = customerService.getAll(page, size, orderBy, direction);
-		log.info("Response: {}", response);
-		return ResponseEntity.ok(response);
+		@RequestParam(name = "direction", defaultValue = "ASC") String direction) {
+
+		return ResponseEntity.ok(customerService.getAll(page, size, orderBy, direction));
+	}
+
+	@PostMapping("")
+	@Operation(summary = "Create a new Customer")
+	public ResponseEntity<CustomerResponseDto> postMethodName(
+		@Valid @RequestBody CreateCustomerRequestDto dto) {
+
+		return ResponseEntity.ok(new CustomerResponseDto(null, null, null, null));
 	}
 }
